ca715834dde21b28ac06f136d11bbef98f7d4d84
amxnese <aminesdrt42@gmail.com>
2023-05-09 07:24:05 +0100

    addedd ptr.c

---
+++ ptr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+    int n;
+    printf("enter the number of integers:  ");
+    scanf("%d",&n);
+    int *ptr = (int*)malloc(n*(sizeof(int)));
+    if(ptr == NULL){
+        printf("memory not available");
+        exit(1);
+    }
+    for(int i=0;i<n;i++){
+        printf("enter an integer:  ");
+        scanf("%d",ptr+i);
+    }
+    for(int i=0;i<n;i++){
+        printf("%d\n",(*ptr)+i);
+    }
+}
