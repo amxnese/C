b3a7fc56e6ee205c54769eae4e61d7fe2688059b
amxnese <aminesdrt42@gmail.com>
2023-10-25 17:12:37 +0100

    Modiefied 'intero1.c' File

--- intero1.c
+++ intero1.c
@@ -5,12 +5,12 @@ int main(void){
         scanf("%d",&number);
         }while(number < 1 || number > 7);
     switch (number){
-    case 1:printf("samedi");break;
+    case 1:printf("samedi");  break;
     case 2:printf("dimanche");break;
-    case 3:printf("lundi");break;
-    case 4:printf("mardi");break;
+    case 3:printf("lundi");   break;
+    case 4:printf("mardi");   break;
     case 5:printf("mercredi");break;
-    case 6:printf("jeudi");break;
+    case 6:printf("jeudi");   break;
     case 7:printf("vendredi");break;
     default:printf("invalid input please try again");break;
     }
